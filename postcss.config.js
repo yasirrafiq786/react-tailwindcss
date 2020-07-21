87156c300bc81ee608cf1c7b878774cc60378ab4
Yasir Malik <yasirrafiq786@gmail.com>
2020-07-21 19:59:56 +1000

    Added Settings in Package.json

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,5 @@
-const tailwindcss = require('tailwindcss');
-module.exports = {
-    plugins: [
-        tailwindcss('./tailwind.js'),
-        require('autoprefixer')
-    ],
-};
+import tailwindcss from 'tailwindcss';
+export const plugins = [
+    tailwindcss('./tailwind.js'),
+    require('autoprefixer')
+];
